ce6aceed86ac60a98dac1756d8555682a5c6d7e2
Ugwuta Kingsley <ugwutakingsley31@gmail.com>
2024-10-03 14:47:46 +0100

    refactor: created a generic onboarding view controller
    This commit adds a generic onboarding view controller that can be reused across multiple onboarding screens

--- Bankey/Bankey/OnboardingViewController.swift
+++ Bankey/Bankey/OnboardingViewController.swift
@@ -12,7 +12,20 @@ class OnboardingViewController: UIViewController {
     let stackView = UIStackView()
     let imageView = UIImageView()
     let label = UILabel()
-
+    let heroImageName: String
+    let titleText: String
+    
+    init(heroImageName: String, titleText: String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         style()
@@ -32,10 +45,10 @@ extension OnboardingViewController {
         
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: "delorean")
+        imageView.image = UIImage(named: heroImageName)
         
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in 1989"
+        label.text = titleText
         label.textAlignment = .center
         label.adjustsFontForContentSizeCategory = true
         label.numberOfLines = 0
